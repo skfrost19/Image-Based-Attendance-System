8cee3adfbbe7f77e9fba66ce344852e1ad3162fc
omnikingzeno <manupande21@gmail.com>
2023-11-30 23:11:54 +0530

    pipeline and styling changes

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-# TEST all the functions here
+""" # TEST all the functions here
 
 from src.components.pipeline import pipeline
+
+if __name__ == "__main__":
+    pipeline() """
+
+from src.components.pipeline import pipeline
+from src.components.generate import extract_faces
+from src.utils import plot_faces
 
 if __name__ == "__main__":
     pipeline()
